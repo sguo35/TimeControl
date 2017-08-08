472e7c919ba7ff0387104006dc0de01c3158b8de
BuildTools <sguo35@gmail.com>
2017-08-07 21:25:25 -0700

    Revert "Implement client"
    This reverts commit 567eae4002542f2def18c8dfaaeec7460ac43e2c.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var ffi = require("ffi");
 var fs = require("fs");
-const express = require('express');
 
 var TimeControlLibrary = ffi.Library('./TimeLibrary', {
     "isIdle": ["bool", []],
@@ -8,46 +7,34 @@ var TimeControlLibrary = ffi.Library('./TimeLibrary', {
 });
 
 var time = {
-    timeRemaining: 60 * 7, //in minutes
+    timeRemaining: 100, //in minutes
     lastTime: new Date().getTime()
 };
 try {
     var fileStatus = fs.statSync("./config.json");
 }
-catch (err) {
+catch(err){
     console.log(err);
     fs.writeFileSync("./config.json", JSON.stringify(time));
 }
 time = JSON.parse(fs.readFileSync("./config.json"));
 
-var checkerFunction = function () {
+var checkerFunction = function() {
     console.log("Checking...");
     console.log(JSON.stringify(time));
-    if (!TimeControlLibrary.isIdle()) {
+    if(!TimeControlLibrary.isIdle()){
         time.timeRemaining--;
         fs.writeFileSync("./config.json", JSON.stringify(time));
     }
-    if (time.lastTime + (1000 * 60 * 60 * 24 * 7 /* 1 week*/) <= (new Date()).getTime()) {
+    if(time.lastTime + (1000 * 60 * 60 * 24 * 7 /* 1 week*/) <= (new Date()).getTime()){
         //if 1 week is up, reset the time
         time.timeRemaining = 60 * 7; //7 hours
         fs.writeFileSync("./config.json", JSON.stringify(time));
     }
-    if (time.timeRemaining < 0) {
+    if(time.timeRemaining < 0){
         TimeControlLibrary.logout();
     }
 }
 
 console.log("Loaded successfully!");
 setInterval(checkerFunction, 1000 * 60 /* 1 minute */);
-
-
-const app = express();
-app.post('/', function (req, res, next) {
-    console.log("Got req!");
-    res.send(JSON.stringify({
-        timeLeft: time.timeRemaining
-    }));
-    next();
-});
-app.listen(4000);
-console.log("Server loaded");
